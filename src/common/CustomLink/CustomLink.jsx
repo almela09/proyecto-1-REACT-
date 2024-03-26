1886c6f6ebea627e1a92e49d9c683bf326e44031
almela09 <145797671+almela09@users.noreply.github.com>
2024-03-26 11:49:02 +0100

    preparing login

--- src/common/CustomLink/CustomLink.jsx
+++ src/common/CustomLink/CustomLink.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom"
 export const CustomLink = ({title, path})=>{
     const navigate = useNavigate()
     return(
-        <div className="linkDesign" onClick={()=> navigate(path)}>
+        <div className="navigateDesign" onClick={()=> navigate(path)}>
             {title}
         </div>
     )
--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -2,17 +2,21 @@ import "./Header.css";
 import { CustomLink } from "../CustomLink/CustomLink";
 
 export const Header = () => {
+  const token = false; //esto esta hardcodeadisimo,aqui va el token del back
   return (
     <div className="headerDesign">
-        <CustomLink 
-        title={"home"} 
-        path={"/"} />
-      <CustomLink 
-      title={"register"} 
-      path={"/register"} />
-      <CustomLink
-       title={"login"} 
-       path={"/login"} />
+      <CustomLink title={"home"} path={"/"} />
+      {token ? (
+        <div className="menu"> 
+          <CustomLink title="Name" path="/login" />
+          <CustomLink title="Log-out" path="/log-out" />
+        </div>
+      ) : (
+        <div className="menu">
+          <CustomLink title="register" path="/register" />
+          <CustomLink title="login" path="/login" />
+        </div>
+      )}
     </div>
   );
 };
